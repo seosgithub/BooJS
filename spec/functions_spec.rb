1b6c72d36b3b2e5114eb3dcb14300f447a7b7fc8
seo <seotownsend@icloud.com>
2015-04-24 21:25:09 -0400

    Added spec for stdout

--- spec/functions_spec.rb
+++ spec/functions_spec.rb
@@ -37,6 +37,40 @@ RSpec.describe "JS Functions" do
     end
   end
 
+  it "console.log writes to stdout" do
+    @err = StringIO.new
+    @out = StringIO.new
+
+    pipe = Open3.popen3("ruby -Ilib ./bin/boojs") do |i, o, e, t|
+      begin
+        Timeout::timeout(5) do
+          i.puts "console.log('hello world')"
+          loop do
+            res = select [o, e], []
+
+            if res[0].include? e
+              @err.write e.readline
+            end
+
+            if res[0].include? o
+              @out.write o.readline
+            end
+          end
+        end
+      rescue Timeout::Error
+      rescue EOFError
+      ensure
+        begin
+          Process.kill(:KILL, t[:pid])
+        rescue Errno::ESRCH
+        end
+      end
+
+      expect(@out.string.strip).to eq("hello world")
+      expect(@err.string.strip).to eq("")
+    end
+  end
+
   it "booPing writes 'pong' to stdout" do
     @out = StringIO.new
 
